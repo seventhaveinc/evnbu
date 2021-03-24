01a40e8412ccc45ede14b101cd5cffdcfd4a34b8
Nykarri <karri.bonez@gmail.com>
2021-03-24 13:30:40 -0400

    added bus and fixed cloged working tree

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './streamer'
+export * from './eventBus'
+export * from './channel'
+export * from './topic'
--- src/topic.d.ts
+++
@@ -1,7 +0,0 @@
-import { Streamer } from './streamer';
-export declare class Topic extends Streamer {
-    readonly channel: string;
-    constructor(name: string, channel: string, maxListeners?: number);
-    emit(payload?: any | undefined): void;
-    destroy(): void;
-}
